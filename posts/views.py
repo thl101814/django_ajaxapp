f26785a67fab7bd005f9f2ffd00380569d4081b1
thl101814 <akihiro-omori@tokyu-hl.jp>
2024-06-20 11:39:13 +0900

    リクエストを処理する

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.views import View
 from posts.models import Post
 from .forms  import PostForm
 
 class IndexView(View):        
   def get(self,request,*args,**kwargs):
-    posts =  Post.objects.all().all().order_by('-created_at')
+    posts =  Post.objects.all()
     form = PostForm()
     return render(request, 'posts/index.html', {'posts': posts, 'form': form})
 
   def post(self, request, *args, **kwargs):
     form = PostForm(request.POST)
     if form.is_valid():
-      form.save()
-      return redirect('posts:index')
+      post = form.save()
+      return JsonResponse({
+        'content': post.content,
+        'created_at': post.created_at.strftime('%Y-%m-%d %H:%M:%S')
+      })
 
 # class CreateView(View):
 #   def get(self, request, *args, **kwargs):
